a2078fa3207094c845ecf8669fd83e8ba3ec10c3
Muhammad Hamza Ashraf <mhamza.141097@gmail.com>
2022-12-26 09:33:23 +0500

    Register Field Boundary logic updated.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import json
-import fiona
+# import fiona
 import geopandas as gpd
 from flask import jsonify, request
 from flask_migrate import Migrate
@@ -16,17 +16,17 @@ from db.models import geoIdsModel, s2CellTokensModel, cellsGeosMiddleModel
 migrate = Migrate(app, db)
 
 
-@app.route('/kml-to-wkt', methods=['POST'])
-def convert_kml_to_wkt():
-    kml_file = request.files.get('kml')
-
-    fiona.supported_drivers['KML'] = 'rw'
-    f = fiona.BytesCollection(bytes(kml_file.content))
-    df = gpd.GeoDataFrame()
-
-    gdf = gpd.read_file(kml_file, driver='KML')
-    poly = gdf.geometry.iloc[0]  # shapely polygon
-    wkt = poly.wkt
+# @app.route('/kml-to-wkt', methods=['POST'])
+# def convert_kml_to_wkt():
+#     kml_file = request.files.get('kml')
+#
+#     fiona.supported_drivers['KML'] = 'rw'
+#     f = fiona.BytesCollection(bytes(kml_file.content))
+#     df = gpd.GeoDataFrame()
+#
+#     gdf = gpd.read_file(kml_file, driver='KML')
+#     poly = gdf.geometry.iloc[0]  # shapely polygon
+#     wkt = poly.wkt
 
 
 @app.route('/register-field-boundary', methods=['POST'])
@@ -38,32 +38,37 @@ def register_field_boundary():
     field_wkt = data.get('wkt')
     resolution_level = data.get('resolution_level')
 
-    # get the L13 indices
-    # s2_index__L13_list is a list of tokens(hex encoded version of the cell id)
-    s2_index__l13_list = S2Service.wkt_to_cell_ids(field_wkt, resolution_level)
+    # get the Different resolution level indices
+    # list against a key (e.g. 13) is a list of tokens(hex encoded version of the cell id)
+    indices = {8: S2Service.wkt_to_cell_ids(field_wkt, 8),
+               13: S2Service.wkt_to_cell_ids(field_wkt, 13),
+               15: S2Service.wkt_to_cell_ids(field_wkt, 15),
+               18: S2Service.wkt_to_cell_ids(field_wkt, 18),
+               19: S2Service.wkt_to_cell_ids(field_wkt, 19),
+               20: S2Service.wkt_to_cell_ids(field_wkt, 20),
+               }
 
-    # fetching the new s2 cell tokens records for Resolution Level 13 in the database
-    records_list_s2_cell_tokens = Utils.records_s2_cell_tokens(s2_index__l13_list)
+    # fetching the new s2 cell tokens records for different Resolution Levels, to be added in the database
+    records_list_s2_cell_tokens_dict = Utils.records_s2_cell_tokens(indices)
 
     # generate the geo_id only for `s2_index__l13_list`
-    geo_id = Utils.generate_geo_id(s2_index__l13_list)
+    geo_id = Utils.generate_geo_id(indices[13])
 
     # lookup the database to see if geo id already exists
     geo_id_exists = Utils.lookup_geo_ids(geo_id)
 
     # if geo id not registered, register it in the database
     if not geo_id_exists:
-        Utils.register_field_boundary(geo_id, s2_index__l13_list, records_list_s2_cell_tokens, resolution_level)
+        geo_data = Utils.register_field_boundary(geo_id, indices, records_list_s2_cell_tokens_dict)
         return jsonify({
             "Message": "Field Boundary registered successfully.",
             "GEO ID": geo_id,
-            "S2 Cell Tokens": s2_index__l13_list
+            "S2 Cell Tokens": geo_data
         })
     else:
         return jsonify({
             "Message": "Field Boundary already registered."
         })
-
 
 @app.route('/fetch-overlapping-fields', methods=['GET'])
 def fetch_overlapping_fields():
