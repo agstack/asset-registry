73f41651c646bc3e4d3279836d7a4853392199ef
Muhammad Hamza Ashraf <mhamza.141097@gmail.com>
2023-02-13 01:48:10 +0500

    CORS added.

--- dbms/__init__.py
+++ dbms/__init__.py
@@ -2,11 +2,13 @@ import os
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
+CORS(app)
 app.config.from_object(os.getenv('APP_SETTINGS'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
